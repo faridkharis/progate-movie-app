1285ea0e147bd7b96d78505619303563bdf60e44
Muhamad Farid Kharismawan <faridkharismawan@gmail.com>
2024-06-27 20:07:01 +0700

    Defines navigation between Favorite and MovieDetail

--- src/navigations/FavoriteStackNavigation.tsx
+++ src/navigations/FavoriteStackNavigation.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "../screens/Home";
 import MovieDetail from "../screens/MovieDetail";
+import Favorite from "../screens/Favorite";
 
 const Stack = createNativeStackNavigator();
-const HomeStackNavigation = (): JSX.Element => {
+const FavoriteStackNavigation = (): JSX.Element => {
   return (
-  <Stack.Navigator initialRouteName="Home">
-    <Stack.Screen name="Home" component={Home} />
+  <Stack.Navigator initialRouteName="Favorite">
+    <Stack.Screen name="Favorite" component={Favorite} />
     <Stack.Screen name="MovieDetail" component={MovieDetail} />
   </Stack.Navigator>
   );
 };
 
-export default HomeStackNavigation;
+export default FavoriteStackNavigation;
--- src/screens/Favorite.tsx
+++ src/screens/Favorite.tsx
@@ -1,10 +1,61 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, FlatList, StyleSheet, } from 'react-native'
+import MovieItem from '../components/movies/MovieItem'
+import type { Movie } from '../types/app'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export default function Favorite(): JSX.Element {
+const Favorite = (): JSX.Element => {
+  const [favoriteMovies, setFavoriteMovies] = useState<Movie[]>([])
+
+  useEffect(() => {
+    void getFavoriteMovies()
+  }, [])
+
+  const getFavoriteMovies = async (): Promise<void> => {
+    try {
+      const favoriteMoviesData = await AsyncStorage.getItem('@FavoriteList')
+      if (favoriteMoviesData !== null) {
+        const movies: Movie[] = JSON.parse(favoriteMoviesData)
+        movies.map(movie => {console.log(movie.title)})
+        setFavoriteMovies(movies)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
   return (
-    <View>
-      <Text>Favorite</Text>
-    </View>
+      <View style={styles.container} >
+
+        <FlatList
+        data={favoriteMovies}
+          renderItem={({ item }) => (
+            <MovieItem
+              movie={item}
+              size={styles.poster}
+              coverType={'backdrop'}
+            />
+          )}
+          numColumns={3}
+          keyExtractor={(item) => item.id.toString()}
+        />
+        
+      </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 'auto',
+    padding: 20,
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  poster: {
+    width: 100,
+    height: 160,
+  },
+})
+
+export default Favorite
